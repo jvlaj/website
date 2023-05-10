c19e86256ac995610b0b884d71580b2579ea1abd
Jason Vlajankov <jvlaj@protonmail.com>
2023-05-10 17:01:37 +1000

    fixed navbar resizing, fixed post formatting on /posts/

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 export default function PostDetail(post) {
   return (
-    <article key={post._id} className="mb-4 gap-6 px-10 hover:underline">
+    <article className="mb-4 gap-6 px-10 hover:underline">
       <Link href={post.slug}>
         <h2 className="font-bold">{post.title}</h2>
         {post.description ?
           ((post.description?.length >= 100) ? (
-            <p>{post.description.slice(0, 100) + "..."}</p>
+            <p className="font-italic text-sm">{post.description.slice(0, 100) + "..."}</p>
           ) : (
-            <p className="font-italic">post.description</p>
+            <p className="font-italic text-sm">post.description</p>
           ))
           :(null)}
       </Link>
